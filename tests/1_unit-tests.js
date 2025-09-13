00861a5f3da1b3377da4b5a4c0d43bfded7cf4e6
dev-front33 <31637994-dev-front33@users.noreply.replit.com>
2025-09-13 18:25:21 +0000

    test : add unit tests for fractional and decimal fractional

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,5 +14,13 @@ suite('Unit Tests', function(){
     assert.equal(convertHandler.getNum('1.2l'), 1.2);
   });
 
+  test('convertHandler should correctly read a fractional input', function() {
+    assert.equal(convertHandler.getNum('3/2km'), 1.5);
+  });
+
+  test('convertHandler should correctly read a fractional input with a decimal', function() {
+    assert.equal(convertHandler.getNum('2.8/4km'), 0.7);
+  });
+
   
 });
