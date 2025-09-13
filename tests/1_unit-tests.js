7eb93696c03fe13426b128c28c891760a3b3bf12
dev-front33 <31637994-dev-front33@users.noreply.replit.com>
2025-09-13 18:13:05 +0000

    test : add unit tests for whole number and decimal

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,4 +6,13 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
 
+  test('convertHandler should correctly read a whole number input', function() {
+    assert.equal(convertHandler.getNum('18km'), 18);
+  });
+
+  test('convertHandler should correctly read a decimal number input', function() {
+    assert.equal(convertHandler.getNum('1.2l'), 1.2);
+  });
+
+  
 });
