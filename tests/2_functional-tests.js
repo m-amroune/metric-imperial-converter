a6689b895ed09e1f1a4fdb8fab813a751325cd4e
dev-front33 <31637994-dev-front33@users.noreply.replit.com>
2025-09-18 07:41:08 +0000

    test : Add functional tests for valid (10L) and invalid (32g) unit conversion

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,35 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  test('Convert a valid input such as 10L: GET request to /api/convert', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: '10L' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, 'L');
+        assert.approximately(res.body.returnNum, 2.64172, 0.1);
+        assert.equal(res.body.returnUnit, 'gal');
+        done();
+      });
+  });
+
+  test('Convert an invalid input such as 32g: GET request to /api/convert', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: '32g' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'invalid unit');
+        done();
+      });
+  });
+
+
+
+  
 });
+
